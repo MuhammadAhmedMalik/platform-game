e25f847921c1041c06bb70cfbe5b4e1e7a79db51
Muhammad Ahmed <muhammadahmed7862468@gmail.com>
2022-09-24 00:59:33 +0500

    First Commit

---
+++ Assets/Scripts/CoinScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinScript : MonoBehaviour
+{
+	private LevelManagerController gameLevelManager;
+    public int coinValue;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameLevelManager = FindObjectOfType<LevelManagerController> ();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    	
+    }
+
+	void OnTriggerEnter2D(Collider2D other){
+        if(other.tag == "player"){
+            gameLevelManager.AddCoins(coinValue);
+            Destroy(gameObject);
+        }
+	}
+}
